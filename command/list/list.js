8e9770f1d9e1bf88a09dc7f85a6ca22bf7aeaf62
Xenilov <tristan.couturier1998@gmail.com>
2020-10-20 15:39:38 +0200

    Add list command :sparkles:

---
+++ command/list/list.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { getAllByUser } = require('../../lib/shortcuts');
+
+const info = {
+  name: 'List',
+  command: 'list',
+  args: null,
+  description: 'List all the shortcuts available for the user'
+};
+
+const action = async (message) => {
+  const author = message.author;
+  try {
+    const shortcuts = getAllByUser(author.id);
+    if (shortcuts.length > 0) {
+      const stringShortcut = shortcuts.map(s => `${s.name} : ${s.emojis.join(' ')}`).join('\n');
+      // Can't use markdown for style, it does not handle emojis very good
+      message.channel.send('[' + author.username + ']\n' + stringShortcut);
+    } else {
+      message.channel.send('Your shortcuts list is now empty.');
+    }
+  } catch (e) {
+    message.channel.send(
+      `An error occured, maybe you never created a shortcut.\nUse _${process.env.PREFIX} create_ to create a shortcut !`
+    );
+  }
+};
+
+module.exports = {
+  info,
+  action
+};
