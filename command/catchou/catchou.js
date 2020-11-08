74111e1771836c1a123f18074f3ffeb2a2f3ceb8
DBric <33903819+DoBrcd@users.noreply.github.com>
2020-11-08 23:54:40 +0100

    Easter egg update 🥚 (#41)
    * Easter egg :egg:
    
    * Remove EasterEgg from help 🥚
    
    * Use db for egg 🥚

--- command/catchou/catchou.js
+++ command/catchou/catchou.js
@@ -1,20 +1,34 @@
 'use strict';
 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('catchou.json');
+const db = low(adapter);
+
+db.defaults({ top3: [] }).write();
+
 const { tryDelete } = require('../../lib/discordjs-utils');
 
 const info = {
   name: 'catchou',
   command: 'catchou',
   args: 'none',
-  description: 'Surprise M*&#F!~?'
+  description: 'egg'
 };
 
 const action = async (message, args) => {
   tryDelete(message);
-  message.channel.send('Top 3 - Nini :');
-  message.channel.send('1. Kevin');
-  message.channel.send('2. Tristan');
-  message.channel.send('3. Willou');
+  const top = db.get('top3').value();
+  let text = 'Top 3 de Nini : \n';
+  text = text.concat(top
+    .map(
+      (c) =>
+        `${c.pos}. ${c.name} (Parce que : ${c.reason})`
+    )
+    .join('\n')
+  );
+  message.channel.send(text);
   message.channel.send('Mais le best des best c\'est Dodo');
 };
 
