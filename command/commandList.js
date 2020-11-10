ff3a4ca03eec489297c0d8267970f67ae7587b8d
DBric <33903819+DoBrcd@users.noreply.github.com>
2020-11-10 15:06:36 +0100

    Update shortcut command ✨ (#40)
    * Update command :poop:
    
    * Optimize update :zap:

--- command/commandList.js
+++ command/commandList.js
@@ -7,10 +7,11 @@ const create = require('./create/create');
 const gif = require('./gif/gif');
 const list = require('./list/list');
 const del = require('./delete/delete');
+const update = require('./update/update');
 const catchou = require('./catchou/catchou');
 
 // Commands
-const commands = [write, clean, react, create, list, gif, del, catchou];
+const commands = [write, clean, react, create, update, del, list, gif, catchou];
 const defaultCommand = react;
 
 // Register commands if they are compliant
